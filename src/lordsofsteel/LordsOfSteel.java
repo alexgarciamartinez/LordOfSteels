ec9a78d5873dc016ef4f4dec44630b0a61d47d6a
Alex Garcia <garcia.martinez.alex@alumnat.copernic.cat>
2023-05-04 10:23:19 +0200

    Personatges creats

--- src/lordsofsteel/LordsOfSteel.java
+++ src/lordsofsteel/LordsOfSteel.java
@@ -11,8 +11,11 @@ package lordsofsteel;
 public class LordsOfSteel {
     public static void main(String[] args) {
         
-        Arma arma = new Arma("Daga", 5, 15);
-        Nan n1 = new Nan(8, 6, 13, 16, 16, arma);
+        Nan n1 = new Nan(8,6, 13,16,16,new Arma("Daga"));
+        Huma h1 = new Huma(13,16,6,10,6,new Arma("Espasa"));
+        Mitja mi1 = new Mitja(9,7,7,15,16,new Arma("Martell"));
+        Maia ma1 = new Maia(7,9,12,15,17,new Arma("Daga"));
+        
         
     }
     
